5003dd5a9fe82a9a67e473d301a05315f42a459a
Stian Soltvedt <soltvedt.stian@gmail.com>
2017-01-16 18:31:56 +0100

    Rewrote entire application - this version is untested.

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # RegistryNuker
 A small C# console application for quickly travling the Registry to delete keys or values containing an unwanted string. 
-Made because an application refused to reinstalled because of hundreds of leftover registry keys from the bad uninstaller.
+Made because an application refused to installe because of hundreds of leftover registry keys from the previous installation.
 
-The console will ask if you want to serach through CURRENT_USER or LOCAL_Machine, and if you want to print matches, just delete all matches, or ask for confirmation for each one.
- 
+The program will ask for the following options before starting:
+
+1. Whether you want to search through CURRENT_USER or LOCAL_MACHINE
+2. If the program should ask for confirmation before deleting each key, delete all immediately, or go read-only
+3. The string to search for
+4. If it should search in value names, value data, or both
+
+**This version was rewritten and has yet to be tested on a live computer.**
--- RegistryNuker/Program.cs
+++ RegistryNuker/Program.cs
@@ -1,155 +1,187 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
 using Microsoft.Win32;
 using static Microsoft.Win32.Registry;
 
 namespace RegistryNuker
 {
-    internal class Program
+    internal class RegistryNuker
     {
+        private static SearchMode searchMode;
+        private static DeleteMode deleteMode;
+        private static string searchString;
+
+        private enum SearchMode
+        {
+            ValueNames,
+            ValueData,
+            Both
+        }
+
+        private enum DeleteMode
+        {
+            Readonly,
+            ConfirmBeforeDelete,
+            DeleteAll
+        }
+
         private static void Main()
         {
+            // Select a base key
             Console.WriteLine("This program will search for a keyword and delete every key that contains it. USE WITH CARE, THIS CAN AND WILL WRECK WINDOWS.");
-            Console.WriteLine("Enter a number: ");
+            Console.WriteLine("Select a base key to search through: ");
             Console.WriteLine("1: CurrentUser");
             Console.WriteLine("2: LocalMachine");
             
             RegistryKey basekey = null;
-            while (basekey == null)
+            var basekeyInput = GetInput(new[] { "1", "2" });
+            switch (basekeyInput)
             {
-                var input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
-                        basekey = CurrentUser;
-                        break;
-                    case "2":
-                        basekey = LocalMachine;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid number");
-                        break;
-                }
+                case "1": basekey = CurrentUser;  break;
+                case "2": basekey = LocalMachine; break;
             }
 
-            Console.WriteLine("Select a mode?");
-            Console.WriteLine("Enter a number: ");
-            Console.WriteLine("1: Dry run - list all matches");
-            Console.WriteLine("2: Manual mode - list and ask if it should delete");
+            // Mode selection
+            Console.WriteLine("Select a mode:");
+            Console.WriteLine("1: Dry run      - list all matches");
+            Console.WriteLine("2: Manual mode  - asks if you want to delete each key");
             Console.WriteLine("3: Nuclear mode - delete all matches without confirmation");
-            var shouldDelete = false;
-            var confirmation = true;
-            var haveShouldDelete = false;
-            while (haveShouldDelete == false)
+            var modeInput = GetInput(new[] { "1", "2", "3" });
+            switch (modeInput)
             {
-                var input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
-                        shouldDelete = false;
-                        confirmation = false;
-                        haveShouldDelete = true;
-                        break;
-                    case "2":
-                        shouldDelete = true;
-                        confirmation = true;
-                        haveShouldDelete = true;
-                        break;
-                    case "3":
-                        shouldDelete = true;
-                        confirmation = false;
-                        haveShouldDelete = true;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid number");
-                        break;
-                }
+                case "1": deleteMode = DeleteMode.Readonly;            break;
+                case "2": deleteMode = DeleteMode.ConfirmBeforeDelete; break;
+                case "3": deleteMode = DeleteMode.DeleteAll;           break;
             }
 
+            // Search string
             Console.WriteLine("Enter a search string to delete matches for. Not case sensitive. BE CAREFUL!!!");
-            var searchstring = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(searchstring))
+            searchString = GetInput().ToLowerInvariant();
+            
+            // Search mode
+            Console.WriteLine("Select a search mode.");
+            Console.WriteLine("1: Search in value names");
+            Console.WriteLine("2: Search in value data");
+            Console.WriteLine("3: Search in both");
+            var searchmodeInput = GetInput(new[] {"1", "2", "3"});
+            switch (searchmodeInput)
             {
-                Console.WriteLine("Not going to delete everything. Goodbye");
-                return;
+                case "1": searchMode = SearchMode.ValueNames; break;
+                case "2": searchMode = SearchMode.ValueData;  break;
+                case "3": searchMode = SearchMode.Both;       break;
             }
 
-            GetValuesContaining(searchstring, basekey, shouldDelete, confirmation);
+            // Start searching
+            GetValuesContaining(basekey);
             Console.WriteLine("Done. Press any key to exit.");
             Console.ReadLine();
         }
 
-        private static void GetValuesContaining(string searchString, RegistryKey key, bool delete = false, bool confirmation = true, bool skipThis = false)
+        /// <summary>
+        /// Calls itself on all subkeys and looks for values matching the search string. Uses all three static variables but does not change them.
+        /// </summary>
+        /// <param name="key">The key to search through.</param>
+        /// <param name="thisKeyIsReadonly">Skips searching/deliting values for this key, useful if readonly.</param>
+        private static void GetValuesContaining(RegistryKey key, bool thisKeyIsReadonly = false)
         {
-            // Repeat for all subkeys
+            // Repeat recursively for all subkeys
             var subkeyNames = key.GetSubKeyNames();
             foreach (var subkeyName in subkeyNames)
             {
+                // Try opening with write access.
                 try
                 {
                     var subkey = key.OpenSubKey(subkeyName, true);
-                    GetValuesContaining(searchString, subkey, delete, confirmation);
+                    GetValuesContaining(subkey);
                 }
                 catch (Exception)
                 {
+                    // Otherwise try opening with read-only access, since we might be able to get write access on subkeys
                     try
                     {
                         var subkey = key.OpenSubKey(subkeyName);
-                        GetValuesContaining(searchString, subkey, delete, confirmation, true);
+                        GetValuesContaining(subkey, true);
                     }
                     catch (Exception)
                     {
-                        // Well shit.
+                        // Or not.
                     }
                 }
             }
 
-            if (skipThis) return;
+            // Don't bother attempting to delete if this key is readonly.
+            if (thisKeyIsReadonly) return;
 
-            // Get values containing
+            // Get values containing search string
             var valueNames = key.GetValueNames();
             foreach (var valueName in valueNames)
             {
-                var value = key.GetValue(valueName, true);
-                
-                // Ignore miss
-                if (!value.ToString().ToLower().Contains(searchString.ToLower())) continue;
+                var valueData = key.GetValue(valueName, true).ToString();
 
-                Console.WriteLine($"{key.Name} | {valueName} | {value}");
-
-                // List mode
-                if (!delete) continue;
-
-                // Delete mode
-                var shouldDelete = true;
-                if (confirmation)
+                switch (searchMode)
                 {
-                    Console.WriteLine("Delete? Y/n");
-                    var responded = false;
-                    while (!responded)
-                    {
-                        var input = Console.ReadLine();
-                        switch (input.ToLower())
-                        {
-                            case "n":
-                                shouldDelete = false;
-                                responded = true;
-                                break;
-                            case "y":
-                                shouldDelete = true;
-                                responded = true;
-                                break;
-                            default:
-                                Console.WriteLine("Invalid answer");
-                                break;
-                        }
-                    }
+                    case SearchMode.ValueNames:
+                        if (!valueName.ToLowerInvariant().Contains(searchString)) continue;
+                        break;
+                    case SearchMode.ValueData:
+                        if (!valueData.ToLowerInvariant().Contains(searchString)) continue;
+                        break;
+                    case SearchMode.Both:
+                        if (!valueData.ToLowerInvariant().Contains(searchString)) continue;
+                        if (!valueName.ToLowerInvariant().Contains(searchString)) continue;
+                        break;
                 }
-                if (shouldDelete) key.DeleteValue(valueName);
+
+                // Always print matching values.
+                Console.WriteLine($"{key.Name} | {valueName} | {valueData}");
+
+                // Next action depends on selected delete mode.
+                switch (deleteMode)
+                {
+                    case DeleteMode.Readonly:
+                        continue;
+
+                    case DeleteMode.ConfirmBeforeDelete:
+                        Console.WriteLine("Delete? Y/n");
+                        var response = GetInput(new[] {"y", "n"});
+                        if (response == "y") key.DeleteValue(valueName);
+                        break;
+
+                    case DeleteMode.DeleteAll:
+                        key.DeleteValue(valueName);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns user input through Console.ReadLine. Does not accept blank input.
+        /// </summary>
+        /// <param name="validInputs">A list of valid inputs. If not null, only these answers will be accepted. All strings should be lowercase.</param>
+        /// <returns>The user's answer in lowercase.</returns>
+        private static string GetInput(string[] validInputs = null)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                
+                // Don't accept blank input.
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Please enter a valid value.");
+                    continue;
+                }
+
+                input = input.ToLowerInvariant();
+
+                // Return here if we don't need to check for valid inputs.
+                if (validInputs == null) return input;
+
+                // Return if the input passes the validity rules.
+                if (validInputs.Contains(input.ToLower())) return input;
+
+                // Otherwise loop again.
+                Console.WriteLine("Please enter a valid value.");
             }
         }
     }
